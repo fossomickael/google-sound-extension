7d3f62b830eab3d66db90bcfda306542a8655c05
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-21 11:40:05 +0200

    play sound

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 chrome.commands.onCommand.addListener(function(command) {
     console.log('Command:', command);
+    const myAudio = new Audio(chrome.runtime.getURL("https://www.cjoint.com/doc/17_04/GDBo22CpPLB_Denis-Brogniart-AH.mp3"));
+    myAudio.play();
   });
