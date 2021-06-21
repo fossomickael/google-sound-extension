8863f0416112215ce0d5827296e20fb4f923578e
fossomickael <82212162+fossomickael@users.noreply.github.com>
2021-06-21 13:34:20 +0200

    random repo

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
 chrome.commands.onCommand.addListener(function(command) {
-    getRandomRepo;
+  getAllRepos().then((repos) => {
+      const randomRepo = oneRepo(repos);
+      repoUrl = randomRepo["html_url"];
+      chrome.tabs.create({ url: repoUrl });
   });
+  
+   
+   
+});
 
+async function getAllRepos() {
+  url = "https://api.github.com/users/fossomickael/repos"
+  const response = await fetch(url);
+  const repos = await response.json();
+  return repos;
+}
 
-async const getRandomRepo = () => {
-    url = "https://api.github.com/users/fossomickael/repos"
-    let response = await fetch(url);
-    console.log(response);
+const oneRepo = (repos) => {
+  return repos[Math.floor(Math.random()*repos.length)];
+ 
 }
