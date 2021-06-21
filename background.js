cf5fb8f04488727efaae4f2dff46af643ac14d9f
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-21 12:06:10 +0200

    random repo

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
-    const newURL = "http://stackoverflow.com/";
-    chrome.tabs.create({ url: newURL });
+    getRandomRepo;
   });
+
+
+async const getRandomRepo = () => {
+    url = "https://api.github.com/users/fossomickael/repos"
+    let response = await fetch(url);
+    console.log(response);
+}
